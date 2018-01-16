f3aaa80c4c2688fdddb2404d97e0ff923305cc80
paul <paul@b7interactive.com>
2018-01-15 18:04:52 -0700

    updates for removing updateData

--- knexfile.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-module.exports = {
-  client: 'mysql',
-  connection: 'mysql://z5bvaktk3yo0sppw:nevb26e1r99779nd@thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g4jvils2n01o4047',
-  useNullAsDefault: true
-}
--- updateData.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# continually runs updateData and retries as necessary until actually finished
-
-until node updateData.js | tee /dev/stderr | grep -m 1 "COMPLETE"; do : ; done
