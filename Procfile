d2bb16f7609c32524948d0e361131c34c45b8a1a
paul <paul@b7interactive.com>
2018-01-15 17:55:34 -0700

    removing db updating stuff from repo

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: nodemon app.js
-db: knex migrate:latest; cd db_sync; sh updateData.sh
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
   client: 'mysql',
-  connection: process.env.JAWSDB_URL,
+  connection: 'mysql://z5bvaktk3yo0sppw:nevb26e1r99779nd@thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g4jvils2n01o4047',
   useNullAsDefault: true
 }
